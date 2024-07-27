102d727dcf7329a2d0666ea44e5ef88782fb3acd
FajarSant <4jar100so@gmail.com>
2024-07-27 23:12:58 +0700

    update

--- src/index.js
+++ src/index.js
@@ -2,44 +2,39 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const path = require("path");
+const bodyParser = require('body-parser');
+
+// Konfigurasi dotenv di awal
+dotenv.config();
 
 const PORT = process.env.PORT || 2000;
 
-const bodyParser = require('body-parser');
 const app = express();
 
+// Middleware untuk parsing JSON dan URL-encoded data
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
-
+// Middleware untuk CORS
 app.use(cors());
-app.use(express.json());
-
-dotenv.config();
-
-// Middleware untuk parsing JSON dan URL-encoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 // Middleware untuk menyajikan file statis dari direktori uploads
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
-const usercontroller = require("./user/user.controller");
-app.use('/users', usercontroller);
-
-const applicationcontroller = require("./application/application.controller");
-app.use('/application', applicationcontroller)
-
-const savejobscontroller = require("./savejobs/savejobs.controllers");
-app.use('/savejobs', savejobscontroller)
-
-const Jobscontroller = require("./Jobs/Jobs.controller");
-app.use('/jobs', Jobscontroller);
-
-const pelatihancontroller = require("./pelatihan/pelatihan.controllers");
-app.use('/pelatihan', pelatihancontroller);
-
+// Import controller
+const userController = require("./user/user.controller");
+const applicationController = require("./application/application.controller");
+const saveJobsController = require("./savejobs/savejobs.controller");
+const jobsController = require("./jobs/jobs.controller");
+const pelatihanController = require("./pelatihan/pelatihan.controller");
 const authController = require("./auth/auth.controller");
+
+// Route
+app.use('/users', userController);
+app.use('/application', applicationController);
+app.use('/savejobs', saveJobsController);
+app.use('/jobs', jobsController);
+app.use('/pelatihan', pelatihanController);
 app.use('/auth', authController);
 
 app.get("/api", (req, res) => {
